cc5eb3259bfddc4bd9513c89f38a38fd9e3739c0
Maxxbay <maxxbay@gmail.com>
2022-09-26 21:37:42 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from 'redux/store';
-import App from 'components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { App } from 'components/App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-07-phonebook">
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
--- src/pages/ChangeContactPage.js
+++
@@ -1,7 +0,0 @@
-import ChangeContactForm from 'components/ChangeContactForm';
-
-function ChangeContactPage() {
-  return <ChangeContactForm />;
-}
-
-export default ChangeContactPage;
